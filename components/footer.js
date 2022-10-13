6325956cbd7c6899f377e40d46b27eb09c7c2677
Alex White <alex.white@hey.com>
2022-10-12 23:11:06 -0400

    Fixing github link

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const navigation = [
   {
     name: "GitHub",
-    href: "#",
+    href: "https://github.com/devalexwhite/rent-my-background",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
